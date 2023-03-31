504ff4b7ea00da139fb637abffbd9e53dc64aef8
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-03-31 11:02:21 +0300

    update
    add controllers: getAll, getById, add

---
+++ routes/api/controllers.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const Joi = require("joi");
+
+const contacts = require("../../models/contacts");
+
+const { HttpError } = require("../../helpers");
+
+const router = express.Router();
+
+const addSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "string.empty": "Name must not be empty",
+  }),
+  email: Joi.string()
+    .email()
+    .pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    .required()
+    .messages({
+      "string.email": "Email mast be valid",
+      "string.pattern.base": "Email does not have a right format",
+    }),
+  phone: Joi.string()
+    .required()
+    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/)
+    .messages({
+      "string.pattern.base":
+        "Phone number must be in the format (XXX) XXX-XXXX",
+    }),
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId: id } = req.params;
+    const result = await contacts.getContactById(id);
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
