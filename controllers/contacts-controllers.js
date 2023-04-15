bbc8dad406fe4bbcd3959e70a5ea5e2247df7e14
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-04-15 19:58:48 +0300

    Update contacts-controllers.js
    get favorite contacts

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -4,9 +4,14 @@ const { ctrlWrapper } = require("../utils");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner },"", { skip, limit }).populate(
+
+  const filter = { owner };
+  if (favorite === 'true') {
+    filter.favorite = true;
+  }
+  const result = await Contact.find(filter,"", { skip, limit }).populate(
     "owner",
     "email"
   );
