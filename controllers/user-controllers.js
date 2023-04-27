183ca125c7bdd438a3560d56908594261d9d5f59
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-04-27 15:41:20 +0300

    Update user-controllers.js

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -69,7 +69,7 @@ const resendVerifyEmail = async (req, res) => {
   const { email } = req.body;
   if (!email) {
     throw HttpError(400, "missing required field email");
-   }
+  }
   const user = await User.findOne({ email });
   if (!user) {
     throw HttpError(404, "Email not found");
@@ -78,18 +78,24 @@ const resendVerifyEmail = async (req, res) => {
     throw HttpError(400, "Verification has already been passed");
   }
 
+  await User.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationToken: null,
+  });
+
   const verifyEmail = {
     to: email,
     subject: "Verify email",
-    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${user.verificationToken}">Click verify email</a>`,
+    html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${user.verificationToken}">Click verify email</a>`,
   };
-
+  
   await sendEmail(verifyEmail);
 
   res.json({
     message: "Verification email was sent",
   });
 };
+
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -169,7 +175,7 @@ const updateAvatar = async (req, res) => {
 module.exports = {
   register: ctrlWrapper(register),
   verify: ctrlWrapper(verify),
-  resendVerifyEmail:ctrlWrapper(resendVerifyEmail),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
